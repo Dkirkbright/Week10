0583bbf14a7863509cf22eb9d79696012f086ab0
Dkirkbright <kird2_13@uni.worc.ac.uk>
2014-12-12 15:57:33 +0000

    changed plugin

--- sample code/js/index.js
+++ sample code/js/index.js
@@ -3,32 +3,30 @@ var notification_count=0;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-
 		//screen.lockOrientation('landscape-primary');
-		screen.orientation.lock('landscape');
-		
+		//screen.orientation.lock('landscape');
 }
 
 $(document).on('pageinit', function() {
 
 	$('#messageButton').on('click', function() {
-		//screen.lockOrientation('portrait');
-		screen.orientation.lock('portrait');
+		screen.lockOrientation('portrait');
+		//screen.orientation.lock('portrait');
 	});
 	
 	$('#dialogButton').on('click', function() {
-		//screen.lockOrientation('landscape-primary');
-		screen.orientation.lock('landscape-primary');
+		screen.lockOrientation('landscape-primary');
+		//screen.orientation.lock('landscape-primary');
 	});
 
 	$('#notificationButton').on('click', function() {
-		//screen.lockOrientation('landscape-secondary');
-		screen.orientation.lock('landscape-secondary');
+		screen.lockOrientation('landscape-secondary');
+		//screen.orientation.lock('landscape-secondary');
 	});
 
 	$('#2notificationButton').on('click', function() {
-		//screen.lockOrientation('portrait-secondary');
-		screen.orientation.lock('portrait-secondary');
+		screen.lockOrientation('portrait-secondary');
+		//screen.orientation.lock('portrait-secondary');
 	});
 
 });
